4e0e01610a0f97c52f26f8ab4a8d8aa0cdbefde5
halima precious <halimaprecious3@gmail.com>
2022-04-18 17:11:17 +0300

    adds class properties

--- src/app/user.ts
+++ src/app/user.ts
@@ -1,2 +1,16 @@
 export class User {
+    constructor(
+        public login:string,
+        public bio:string,
+        public html_url:string, 
+        public avatar_url:string, 
+        public id:number,
+        public followers:string, 
+        public following:string, 
+        public public_repos:string,
+        public created_at:Date,
+        public location:string,
+        public email:string
+        ){
+}
 }
