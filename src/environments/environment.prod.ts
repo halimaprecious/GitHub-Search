63ab1dc5584d8b2ff7b0fa14ac1e327936bec998
halima precious <halimaprecious3@gmail.com>
2022-04-18 14:57:45 +0300

    add apikey

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  apiKey:'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'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiKey:'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'
 };
 
 /*
