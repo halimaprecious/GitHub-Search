1679d027fc737257e401b564bc2997e93f519240
halima precious <halimaprecious3@gmail.com>
2022-04-18 23:42:03 +0300

    fix:changed file paths

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './Components/home/home.component';
+import { ProfilesComponent } from './Components/profile/profile.component';
+import { RepoComponent } from './Components/repo/repo.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+{path:'',component: HomeComponent},
+
+{path:'Profiles',component: ProfilesComponent},
+{path:'Home', component:HomeComponent},
+{path: 'Repositories', component: RepoComponent},];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
