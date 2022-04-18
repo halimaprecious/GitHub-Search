3928391321cc3c49629478806a267d6e1986cc0f
halima precious <halimaprecious3@gmail.com>
2022-04-18 15:30:04 +0300

    import components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './Components/nav/nav.component';
+import { HomeComponent } from './Components/home/home.component';
+import { ProfileComponent } from './Components/profile/profile.component';
+import { RepoComponent } from './Components/repo/repo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    HomeComponent,
+    ProfileComponent,
+    RepoComponent
   ],
   imports: [
     BrowserModule,
